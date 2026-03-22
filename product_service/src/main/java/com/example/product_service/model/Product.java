81062d60aaa68188c5943d9ba6b7c6a835255b67
Dev jhawar <devjhawar8181@gmail.com>
2026-03-22 15:55:10 +0530

    Add : All files related to Product service

---
+++ product_service/src/main/java/com/example/product_service/model/Product.java
@@ -0,0 +1,11 @@
+package com.example.product_service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data @Setter @Getter
+@AllArgsConstructor
+public class Product {
+}
